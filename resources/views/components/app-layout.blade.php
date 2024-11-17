a3bec65ef98552fc18c7abb3cc39815a58338420
amine taoufik <ataoufik031@gmail.com>
2024-11-17 21:51:58 +0100

    responsive dyal soins o product o khdmt chi blanat khrin kano makhdaminch

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -7,14 +7,15 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <title>{{ $title ?? 'Dashboard' }}</title>
+    
+    <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    <link href="admin_assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
-    <link rel="shortcut icon" type="image/x-icon" href="admin_assets/img/tab-icon.png">
+    
+    <!-- Tailwind CSS -->
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    
+    <!-- Local Assets -->
+    <link rel="shortcut icon" href="{{ asset('admin_assets/img/tab-icon.png') }}">
     <link href="{{ asset('admin_assets/css/sb-admin-2.min.css') }}" rel="stylesheet">
 </head>
 <body id="page-top">
@@ -39,13 +40,10 @@
     <a class="scroll-to-top rounded" href="#page-top">
         <i class="fas fa-angle-up"></i>
     </a>
-    <script src="admin_assets/vendor/jquery/jquery.min.js"></script>
-    <script src="admin_assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="admin_assets/vendor/jquery-easing/jquery.easing.min.js"></script>
-    <script src="admin_assets/js/sb-admin-2.min.js"></script>
-    <script src="admin_assets/vendor/chart.js/Chart.min.js"></script>
-    <script src="admin_assets/js/demo/chart-area-demo.js"></script>
-    <script src="admin_assets/js/demo/chart-pie-demo.js"></script>
+    
+    <!-- Scripts -->
+    <script src="{{ asset('admin_assets/vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('admin_assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('admin_assets/js/sb-admin-2.min.js') }}"></script>
 </body>
-
 </html>
